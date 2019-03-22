4741f76a25d7080c21ac272a4b638c1f3c7f70a7
Anatoly <AnatoliySokolov1981@gmail.com>
2019-03-22 19:38:47 +0300

    fix: дДобавлены стили из онлайн магазина

--- lesson1/main.js
+++ lesson1/main.js
@@ -2,17 +2,31 @@
 
 (function () {
   const goods = [
-    {title: 'Shirt', price: 150},
-    {title: 'Socks', price: 50},
-    {title: 'Jacket', price: 350},
-    {title: 'Shoes', price: 250},
+    {title: 'mango people t-shirt1', price: 52},
+    {title: 'mango people t-shirt2', price: 52},
+    {title: 'mango people t-shirt3', price: 52},
+    {title: 'mango people t-shirt4', price: 52},
+    {title: 'mango people t-shirt5', price: 52},
+    {title: 'mango people t-shirt6', price: 52}
   ];
 
-  const renderGoodsItem = (title, price) => `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+  const renderGoodsItem = (title, price) =>
+    `<li class="product__item item-card">
+       <a class="item-card__link"href="#">
+         <h3 class="item-card__item-title">${title}</h3>
+         <p class="item-card__price">${price}</p>
+         <p class="item-card__stars"></p>
+         <img class="item-card__img" src="img/product__img1.jpg" width="263" height="281" alt="mango people t-shirt">
+       </a>
+       <button class="item-card__cart" type="button">
+        <span>Add to Cart</span>
+       </button>
+    </li>`;
+
 
   const renderGoodsList = list => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+    document.querySelector('.product__list').innerHTML = goodsList.join('');
   };
 
   renderGoodsList(goods);
