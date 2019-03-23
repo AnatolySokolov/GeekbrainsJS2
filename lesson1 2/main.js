399bbb32fb337b4565fec6367a86e9d062e73d19
Anatoly <AnatoliySokolov1981@gmail.com>
2019-03-23 18:09:17 +0300

    task1 complete

--- lesson1 2/main.js
+++ lesson1 2/main.js
@@ -1,20 +1,18 @@
 'use strict';
 
 (function () {
-  const goods = [
-    {title: 'mango people t-shirt1', price: 52},
-    {title: 'mango people t-shirt2', price: 52},
-    {title: 'mango people t-shirt3', price: 52},
-    {title: 'mango people t-shirt4', price: 52},
-    {title: 'mango people t-shirt5', price: 52},
-    {title: 'mango people t-shirt6', price: 52}
-  ];
 
-  const renderGoodsItem = (title, price) =>
-    `<li class="product__item item-card">
-       <a class="item-card__link"href="#">
-         <h3 class="item-card__item-title">${title}</h3>
-         <p class="item-card__price">${price}</p>
+  class GoodsItem {
+    constructor(title, price) {
+      this.title = title;
+      this.price = price;
+    }
+
+    render() {
+      return `<li class="product__item item-card">
+       <a class="item-card__link" href="#">
+         <h3 class="item-card__item-title">${this.title}</h3>
+         <p class="item-card__price">${this.price}</p>
          <p class="item-card__stars"></p>
          <img class="item-card__img" src="img/product__img1.jpg" width="263" height="281" alt="mango people t-shirt">
        </a>
@@ -22,14 +20,98 @@
         <span>Add to Cart</span>
        </button>
     </li>`;
+    }
+  }
 
+  class GoodList {
+    constructor() {
+      this.goods = [];
+    }
 
-  const renderGoodsList = list => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.product__list').innerHTML = goodsList.join('');
-  };
+    fetchGoods() {
+      this.goods = [
+        {title: 'mango people t-shirt1', price: 52},
+        {title: 'mango people t-shirt2', price: 52},
+        {title: 'mango people t-shirt3', price: 52},
+        {title: 'mango people t-shirt4', price: 52},
+        {title: 'mango people t-shirt5', price: 52},
+        {title: 'mango people t-shirt6', price: 52}
+      ];
+    }
 
-  renderGoodsList(goods);
+    render() {
+      let listHtml = '';
+      this.goods.forEach(good => {
+        const goodItem = new GoodsItem(good.title, good.price);
+        listHtml += goodItem.render();
+      });
+      document.querySelector('.product__list').innerHTML = listHtml;
+    }
+  }
+
+  class CartItem {
+    constructor(title, price, img) {
+      this.title = title;
+      this.price = price;
+      this.img = img;
+    }
+
+    render() {
+      return `<li class="cart__item">
+      <a class="cart__link" href="#">
+        <div>
+          <h3 class="cart__title">${this.title}</h3>
+          <div class="cart__rating"></div>
+          <p class="cart__quantity-and-price">
+            <span>1</span>
+            <span class="cart__letter-decoration"> x </span>
+            <span>$ ${this.price}</span>
+          </p>
+        </div>
+        <img class="cart__img" src="${this.img}"  width="72" height="85" alt="#">
+      </a>
+      <button class="cart__delete" type="button">
+        <span class="visually-hidden">Delete the item from cart</span>
+      </button>
+    </li>`;
+    }
+  }
+
+  class Cart {
+    constructor() {
+      this.list = [];
+    }
+
+    add() {
+      // добваление товара в корзину
+      this.list = [
+        {title: 'rebox zane', price: 250, imgUrl: 'img/cart__product1.jpg'},
+        {title: 'rebox zane', price: 250, imgUrl: 'img/cart__product2.jpg'}
+      ];
+    }
+    remove() {
+      // удаление товара из корзины
+    }
+    clear() {
+      // отчистка корзины
+      this.list = [];
+    }
+    render() {
+      let listHtml = '';
+      this.list.forEach(item => {
+        const cartItem = new CartItem(item.title, item.price, item.imgUrl);
+        listHtml += cartItem.render();
+      });
+      document.querySelector('.cart__list').innerHTML = listHtml;
+    }
+  }
+
+  const list = new GoodList();
+  list.fetchGoods();
+  list.render();
+
+  const cart = new Cart();
+  cart.add();
+  cart.render();
+
 })();
-
-
