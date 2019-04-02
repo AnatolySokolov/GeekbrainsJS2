0020442858b671974fa71da4bfbc2dacca4e82c5
Anatoly <AnatoliySokolov1981@gmail.com>
2019-04-02 23:44:20 +0300

    task2 complete

--- public/main.js
+++ public/main.js
@@ -2,91 +2,13 @@
 
 const DATA_URL = 'data';
 
-// class CartItem {
-//   constructor(title, price, img, id) {
-//     this.title = title;
-//     this.price = price;
-//     this.img = img;
-//     this.id = id;
-//   }
-//
-//   render() {
-//     return `<li class="cart__item">
-//       <a class="cart__link" href="#">
-//         <div>
-//           <h3 class="cart__title">{{good.title}}</h3>
-//           <div class="cart__rating"></div>
-//           <p class="cart__quantity-and-price">
-//             <span>1</span>
-//             <span class="cart__letter-decoration"> x </span>
-//             <span>$ ${this.price}</span>
-//           </p>
-//         </div>
-//         <img class="cart__img" src="${this.img}"  width="72" height="85" alt="#">
-//       </a>
-//       <button class="cart__delete" data-product-id=${this.id} type="button">
-//         <span class="visually-hidden">Delete the item from cart</span>
-//       </button>
-//     </li>`;
-//   }
-// }
-
-// class Cart {
-//   constructor() {
-//     this.list = [];
-//   }
-//
-//   add(id) {
-//     // добваление товара в корзину по id-атрибуту
-//     const checkItem = this.list.find(item => item.productId === id);
-//     if (!checkItem) {
-//       goods.find(item => {
-//         if (item.productId === id) {
-//           this.list.push(item);
-//         }
-//       });
-//     }
-//   }
-//
-//   remove(id) {
-//     // удаление товара из корзины по id-атрибуту
-//     this.list.some((item, index) => {
-//       if (item.productId === id) {
-//         this.list.splice(index, 1);
-//       }
-//     });
-//   }
-//
-//   clear() {
-//     // отчистка корзины
-//     this.list = [];
-//     document.querySelector('#clear-cart').removeEventListener('click', onClearCartButtonClick);
-//   }
-//
-//   countTotalCost() {
-//     // подсчет общей стоимость корзины
-//     return this.list.reduce((acc, item) => acc + item.price, 0);
-//   }
-//
-//   render() {
-//     const reducer = (acc, item) => {
-//       const cartItem = new CartItem(item.title, item.price, item.imgUrl, item.productId);
-//       return acc + cartItem.render();
-//     };
-//     document.querySelector('.cart__list').innerHTML = this.list.reduce(reducer, '');
-//     document.querySelector('.cart__list').addEventListener('click', onDeleteButtonClick);
-//     document.querySelector('#clear-cart').addEventListener('click', onClearCartButtonClick);
-//   }
-// }
-
-// const cart = new Cart();
-
 const app = new Vue({
   el: '#app',
   data: {
     goods: [],
     filteredGoods: [],
-    searchLine: ''
+    searchLine: '',
+    cart: []
   },
   methods: {
     sendRequest(url) {
@@ -99,6 +21,50 @@ const app = new Vue({
             throw new Error('Network response was not ok.');
           })
         .catch(error => console.log(error.message));
+    },
+    addToCart(id) {
+      const checkItem = this.cart.find(item => item.productId === id);
+      if (!checkItem) {
+        this.filteredGoods.find(item => {
+          if (item.productId === id) {
+            this.cart.push(item);
+          }
+        });
+      }
+    },
+    removeFromCart(id) {
+      this.cart.some((item, index) => {
+        if (item.productId === id) {
+          this.cart.splice(index, 1);
+        }
+      });
+    },
+    clearCart() {
+      this.cart = [];
+    },
+    countTotalCost() {
+      return this.cart.reduce((acc, item) => acc + item.price, 0);
+    },
+    onAddToCartButtonClick(e) {
+      let id;
+
+      if (e.target.classList.contains('item-card__cart')) {
+        id = e.target.dataset.productId;
+      } else if (e.target.classList.contains('item-card__cart-text')) {
+        id = e.target.parentElement.dataset.productId;
+      }
+      if (id) {
+        this.addToCart(id);
+      }
+    },
+    onDeleteButtonClick(e) {
+      if (e.target.classList.contains('cart__delete')) {
+        const id = e.target.dataset.productId;
+        this.removeFromCart(id);
+      }
+    },
+    onCartButtonClick() {
+      document.querySelector('.cart__wrapper').classList.toggle('cart__wrapper--closed');
     }
   },
   mounted() {
