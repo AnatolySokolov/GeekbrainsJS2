0b07e41fc5726d7c8f3842cb5568103d72c9830b
Anatoly <AnatoliySokolov1981@gmail.com>
2019-04-07 16:38:25 +0300

    task 2 complete

--- public/main.js
+++ public/main.js
@@ -1,3 +1,17 @@
+'use strict';
+
+const sendRequest = url => {
+  return fetch(url)
+    .then(
+      response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+    .catch(error => console.log(error.message));
+};
+
 Vue.component('product-item', {
   props: {
     good: Object
@@ -8,13 +22,18 @@ Vue.component('product-item', {
         <h3 class="item-card__item-title">{{good.title}}</h3>
         <p class="item-card__price">{{good.price}}</p>
         <p class="item-card__stars"></p>
-        <img class="item-card__img" :src="good.imgUrl" width="263" height="281" alt="mango people t-shirt">
+        <img class="item-card__img" :src="good.imgUrl" width="263" height="281" :alt="good.title">
       </a>
-      <button class="item-card__cart" type="button" :data-product-id="good.productId">
+      <button class="item-card__cart" type="button" @click="onAddToCartButtonClick(good)">
         <span class="item-card__cart-text">Add to Cart</span>
       </button>
     </li>
-  `
+  `,
+  methods: {
+    onAddToCartButtonClick(item) {
+      eventEmitter.$emit('transmitItemFromProductItem', item);
+    }
+  }
 });
 
 Vue.component('product-list', {
@@ -33,28 +52,15 @@ Vue.component('product-list', {
       filteredGoods: [],
     };
   },
-  methods: {
-    sendRequest(url) {
-      return fetch(url)
-        .then(
-          response => {
-            if (response.ok) {
-              return response.json();
-            }
-            throw new Error('Network response was not ok.');
-          })
-        .catch(error => console.log(error.message));
-    }
-  },
   mounted() {
-    this.sendRequest(this.DATA_URL)
+    sendRequest(this.DATA_URL)
       .then(goods => {
         this.goods = goods;
         this.filteredGoods = goods;
       });
   },
   created() {
-    eventEmitter.$on('transmitDataFromSearchLine', query => {
+    eventEmitter.$on('transmitDataFromSearchComponent', query => {
       const regExp = new RegExp(query, 'i');
       this.filteredGoods = this.goods.filter(item => regExp.test(item.title));
     });
@@ -75,8 +81,98 @@ Vue.component('search-component', {
   },
   methods: {
     onSearchButtonClick() {
-      eventEmitter.$emit('transmitDataFromSearchLine', this.searchLine);
+      eventEmitter.$emit('transmitDataFromSearchComponent', this.searchLine);
     }
+  }
+});
+
+Vue.component('cart-item', {
+  props: {
+    item: Object
+  },
+  template: `
+    <li class="cart__item">
+    <a class="cart__link" href="#">
+      <div>
+        <h3 class="cart__title">{{item.title}}</h3>
+        <div class="cart__rating"></div>
+        <p class="cart__quantity-and-price">
+          <span>{{item.quantity}}</span>
+          <span class="cart__letter-decoration"> x </span>
+          <span>$ {{item.price}}</span>
+        </p>
+      </div>
+      <img class="cart__img" :src="item.imgUrl" width="72" height="85" :alt="item.title">
+    </a>
+    <button class="cart__delete" type="button" @click="onDeleteButtonClick(item)">
+      <span class="visually-hidden">Delete the item from cart</span>
+    </button>   </li>
+  `,
+  methods: {
+    onDeleteButtonClick(item) {
+      this.$emit('onDelButtonClick', item);
+    }
+  }
+});
+
+Vue.component('cart-component', {
+  template: `
+    <div class="cart__container">
+      <button class="cart-button" type="button" @click="onCartButtonClick">Корзина</button>
+      <div class="cart__wrapper cart__wrapper--closed">
+        <ul class="cart__list">
+          <cart-item v-for="item in cart" :item="item" :key="item.productId" @onDelButtonClick="removeItem"></cart-item>
+        </ul>
+        <p>Итого: {{countTotalCost()}}</p>
+        <button class="cart__button" type="button" @click="sendCart">Отправить на сервер</button>
+        <button class="cart__button" type="button" @click="clearCart">Отчистить корзину</button>
+      </div>
+    </div>
+  `,
+  data() {
+    return {
+      CART_URL: 'cart',
+      cart: []
+    };
+  },
+  methods: {
+    onCartButtonClick() {
+      document.querySelector('.cart__wrapper').classList.toggle('cart__wrapper--closed');
+    },
+    removeItem(item) {
+      const i = this.cart.indexOf(item);
+      this.cart.splice(i, 1);
+    },
+    clearCart() {
+      this.cart = [];
+      document.querySelector('.cart__wrapper').classList.add('cart__wrapper--closed');
+    },
+    countTotalCost() {
+      return this.cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    },
+    sendCart() {
+      // неправильно работает. поправить
+      // fetch(this.CART_URL, {
+      //   method: 'post',
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify(this.cart)
+      // });
+    }
+  },
+  mounted() {
+    sendRequest(this.CART_URL)
+      .then(goods => this.cart = goods);
+  },
+  created() {
+    eventEmitter.$on('transmitItemFromProductItem', item => {
+      if (this.cart.indexOf(item) === -1) {
+        this.cart.push(item);
+      } else {
+        item.quantity++;
+      }
+    });
   }
 });
 
