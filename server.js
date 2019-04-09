a259ff5a97ebade2500e8813dafcebff8a1a470f
Anatoly <AnatoliySokolov1981@gmail.com>
2019-04-09 22:44:54 +0300

    task3 complete

--- server.js
+++ server.js
@@ -21,9 +21,33 @@ app.get('/cart', (req, res) => {
   });
 });
 
-app.put('/cart', (req) => {
+app.put('/cart', req => {
   fs.writeFile('./db/cart.json', JSON.stringify(req.body), err => {
     if (err) return console.log(err);
+  });
+
+  const date = new Date();
+  const dataOptions = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+  };
+  const stats = {
+    date: date.toLocaleString('ru', dataOptions),
+    action: req.body.length ? 'корзина обновлена' : 'корзина удалена'
+  };
+
+  fs.readFile('stats.json', 'utf-8', (err, data) => {
+    if (err) return console.log(err);
+
+    const arr = JSON.parse(data);
+    arr.push(stats);
+
+    fs.writeFile('stats.json', JSON.stringify(arr), err => {
+      if (err) return console.log(err);
+    });
   });
 });
 
